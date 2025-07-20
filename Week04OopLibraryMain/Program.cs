24b66f079e9a4e9951c7f4c656caabed7c85447d
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-07-20 13:59:25 +0300

    Add project files.

---
+++ Week04OopLibraryMain/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Week04OopLibraryMain;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        
+        Week04BookClassification book1 = new Week04BookClassification();
+        Week04BookClassification book2 = new Week04BookClassification("Adı Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi");
+        Week04BookClassification book3 = new Week04BookClassification("Bir Kitap", "Ahmet", "Yılmaz", 320, "Alfa Yayınları");
+        Week04BookClassification book4 = new Week04BookClassification("Başka Bir Kitap", "Ayşe", "Karan", 215, "Beta Yayınları");
+        Week04BookClassification book5 = new Week04BookClassification("Daha Başka Kitap", "Mehmet", "Demir", 450, "Gama Yayınları");
+        Week04BookClassification book6 = new Week04BookClassification("Son Kitap", "Elif", "Şahin", 275, "Delta Yayınları");
+        Week04BookClassification book7 = new Week04BookClassification("Örnek Kitap", "Selin", "Yurt", 198, "Epsilon Yayınları");
+
+
+        book1.DisplayBookInfo();
+        book2.DisplayBookInfo();
+        book3.DisplayBookInfo();
+        book4.DisplayBookInfo();
+        book5.DisplayBookInfo();
+        book6.DisplayBookInfo();
+        book7.DisplayBookInfo();
+    }
+}
+// Class: Week04BookClassification adında bir sınıf oluşturduk, kitap nesnelerini temsil eder.
+// Property: Title, AuthorName gibi özellikler kitapla ilgili bilgileri tutar.
+// Constructor: Sınıftan yeni nesne oluşturulduğunda ilk çalışan özel metotlardır.
+// New: Bir sınıftan nesne oluşturmak için 'new' anahtar kelimesi kullanılır.
---
+++ Week04OopLibraryMain/Week04BookClassification.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04OopLibraryMain
+{
+    internal class Week04BookClassification
+    {
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorSurname { get; set; }
+        public int BookPages { get; set; }
+        public string BookFirm { get; set; }
+        public DateTime BookRecordDate { get; set; }
+
+        public Week04BookClassification()
+        {
+            Title = "Default";
+            AuthorName = "Default";
+            AuthorSurname = "Default";
+            BookPages = 0;
+            BookFirm = "Default";
+            BookRecordDate = DateTime.Now;
+        }
+
+        public Week04BookClassification( string title, string authorName, string authorSurname, int bookPages, string bookFirm)
+        {
+            Title = title;
+            AuthorName = authorName;
+            AuthorSurname = authorSurname;
+            BookPages = bookPages;
+            BookFirm = bookFirm;
+            BookRecordDate = DateTime.Now;
+
+        }
+        public void DisplayBookInfo ()
+        {
+            Console.WriteLine($"{AuthorName} {AuthorSurname} yazarının {BookPages} sayfalık, {BookFirm} yayınevinden çıkan {Title} kitabını oluşturunuz. - {BookRecordDate}");
+        }
+
+
+    }
+}
