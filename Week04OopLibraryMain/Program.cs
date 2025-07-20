376e025f19dbaa387eeb305512dfa407035362f1
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-07-20 14:06:09 +0300

    With comments

--- Week04OopLibraryMain/Program.cs
+++ Week04OopLibraryMain/Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using Week04OopLibraryMain;
 
-class Program
+
+class Program // Main program class
 {
-    static void Main(string[]args)
+    static void Main(string[]args) // Main method the beggining point of the program
     {
         
-        Week04BookClassification book1 = new Week04BookClassification();
-        Week04BookClassification book2 = new Week04BookClassification("Adı Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi");
-        Week04BookClassification book3 = new Week04BookClassification("Bir Kitap", "Ahmet", "Yılmaz", 320, "Alfa Yayınları");
-        Week04BookClassification book4 = new Week04BookClassification("Başka Bir Kitap", "Ayşe", "Karan", 215, "Beta Yayınları");
+        Week04BookClassification book1 = new Week04BookClassification(); // Default constructor is called
+        Week04BookClassification book2 = new Week04BookClassification("Adı Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi"); // Parameterized constructor is called with specific values
+        Week04BookClassification book3 = new Week04BookClassification("Bir Kitap", "Ahmet", "Yılmaz", 320, "Alfa Yayınları"); // Another parameterized constructor is called with different values
+        Week04BookClassification book4 = new Week04BookClassification("Başka Bir Kitap", "Ayşe", "Karan", 215, "Beta Yayınları"); // Another parameterized constructor is called with different values
         Week04BookClassification book5 = new Week04BookClassification("Daha Başka Kitap", "Mehmet", "Demir", 450, "Gama Yayınları");
         Week04BookClassification book6 = new Week04BookClassification("Son Kitap", "Elif", "Şahin", 275, "Delta Yayınları");
         Week04BookClassification book7 = new Week04BookClassification("Örnek Kitap", "Selin", "Yurt", 198, "Epsilon Yayınları");
 
 
-        book1.DisplayBookInfo();
-        book2.DisplayBookInfo();
-        book3.DisplayBookInfo();
+        book1.DisplayBookInfo(); // Display information of the default book
+        book2.DisplayBookInfo(); // Display information of the first parameterized book
+        book3.DisplayBookInfo(); // Display information of the second parameterized book
         book4.DisplayBookInfo();
         book5.DisplayBookInfo();
         book6.DisplayBookInfo();
--- Week04OopLibraryMain/Week04BookClassification.cs
+++ Week04OopLibraryMain/Week04BookClassification.cs
@@ -4,38 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Week04OopLibraryMain
+namespace Week04OopLibraryMain 
 {
-    internal class Week04BookClassification
+    internal class Week04BookClassification // Week04BookClassification class represents a book with various properties and methods to display its information.
     {
-        public string Title { get; set; }
-        public string AuthorName { get; set; }
-        public string AuthorSurname { get; set; }
-        public int BookPages { get; set; }
-        public string BookFirm { get; set; }
-        public DateTime BookRecordDate { get; set; }
+        public string Title { get; set; } // Title of the book
+        public string AuthorName { get; set; } // Author's first name
+        public string AuthorSurname { get; set; } // Author's last name
+        public int BookPages { get; set; } // Number of pages in the book
+        public string BookFirm { get; set; } // Publishing firm of the book
+        public DateTime BookRecordDate { get; set; } // Date when the book record was created
 
-        public Week04BookClassification()
+        public Week04BookClassification() // Default constructor initializes the book with default values
         {
-            Title = "Default";
-            AuthorName = "Default";
+            Title = "Default"; // Default title for the book
+            AuthorName = "Default"; // Default author's first name
             AuthorSurname = "Default";
             BookPages = 0;
             BookFirm = "Default";
             BookRecordDate = DateTime.Now;
         }
 
-        public Week04BookClassification( string title, string authorName, string authorSurname, int bookPages, string bookFirm)
+        public Week04BookClassification( string title, string authorName, string authorSurname, int bookPages, string bookFirm) // Parameterized constructor allows setting specific values for the book properties
         {
-            Title = title;
-            AuthorName = authorName;
+            Title = title; // Title of the book 
+            AuthorName = authorName; // Author's first name
             AuthorSurname = authorSurname;
             BookPages = bookPages;
             BookFirm = bookFirm;
             BookRecordDate = DateTime.Now;
 
         }
-        public void DisplayBookInfo ()
+        public void DisplayBookInfo () // Method to display the book's information in a formatted string
         {
             Console.WriteLine($"{AuthorName} {AuthorSurname} yazarının {BookPages} sayfalık, {BookFirm} yayınevinden çıkan {Title} kitabını oluşturunuz. - {BookRecordDate}");
         }
